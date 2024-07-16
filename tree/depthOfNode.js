42e4d85d3ab41dc970e09edb6bc22d8cbcc7551a
Kishan T A <kishantashok@gmail.com>
2024-07-16 14:30:18 +0530

    finding the depth of each nodes in the tree

--- tree/depthOfNode.js
+++ tree/depthOfNode.js
@@ -25,10 +25,17 @@ class BinaryTree {
         if (leftDepth !== -1) return leftDepth;
         return this.getDepth(node.right, value, depth + 1);
     }
+    getDepths(node,depth=0,depths = {}){
+        if(!node) return;
+        depths[node.value] = depth;
+        this.getDepths(node.left,depth+1,depths);
+        this.getDepths(node.right,depth+1,depths);
+        return depths;
+    }
 }
 
 let tree = new BinaryTree();
 // Assume nodes are inserted here
 tree.root = new Node(1);tree.root.left = new Node(2);tree.root.right = new Node(3);tree.root.left.left = new Node(4);tree.root.left.right = new Node(5);tree.root.right.left = new Node(6);tree.root.right.right = new Node(7);
-console.log(tree.getDepth(tree.root, 6));
-        
+console.log('depths of the given value is :',tree.getDepth(tree.root, 6));
+        console.log('depth of all is :',tree.getDepths(tree.root));
